f1d77b9ca5986f707bdb904c739483c0ca90fd9c
syam <syam@fission>
2016-02-05 12:45:53 +0530

    Bussiness card

---
+++ Assignment 3/businesscard1.js
@@ -0,0 +1,13 @@
+var app = angular.module('myApp', []);
+app.controller('businesscntrl', function($scope) {
+	$scope.name="SyamSandeep";
+	$scope.occ="software engineer";
+	$scope.logo="image.png";
+	$scope.email="syam.sandeep@fissionlabs.in";
+	$scope.personallink1="www.google.com";
+	$scope.personallink2="www.facebook.com";
+});
+	function save(){
+		
+		alert("0k");
+	}
